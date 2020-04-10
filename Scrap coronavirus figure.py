5ef8a260c0e374d756687c633042ef48576e78cc
ncytse <40912330+ncytse@users.noreply.github.com>
2020-04-10 20:13:33 +0800

    Update Scrap coronavirus figure.py

--- Scrap coronavirus figure.py
+++ Scrap coronavirus figure.py
@@ -7,13 +7,13 @@ url = "https://www.worldometers.info/coronavirus/"
 req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 # this is the html from the given url
 html = urllib.request.urlopen(req).read()
-
 soup = BeautifulSoup(html, 'html.parser')
 
 column_headers = [th.getText() for th in 
                   soup.findAll('tr', limit=2)[0].findAll('th')] #[0] means first row of table
 
-data_rows = soup.findAll('tr')[1:] #[1:] means 2nd row of table onward
+table = soup.find('table', id='main_table_countries_today')
+data_rows = table.findAll('tr')[1:] #[1:] means 2nd row of table onward
 type(data_rows)  # now we have a list of table rows
 
 country_data = []  # create an empty list to hold all the data
@@ -30,7 +30,18 @@ for i in range(len(data_rows)):  # for each table row
     country_data.append(country_row)
 
 df = pd.DataFrame(country_data, columns=column_headers)
-df = df[df['Country,Other'] != 'Total:']
-df.drop_duplicates(subset ="First Name", keep = 'first', inplace = True) 
-df.replace(r'\\n', '', regex=True, inplace=True)
+df.shape
+
+df.columns = [w.replace('\n', '') for w in df.columns]
+#df = df.dropna(how='all')
+df['TotalCases']=df['TotalCases'].str.replace(',', '').astype(int)
+#df.drop_duplicates(subset ='Country,Other', keep = 'first', inplace = True)
+df = df[~df['Country,Other'].str.contains('Total')]
+world_index=df.index[df['Country,Other'] == 'World'].tolist()[0] 
+df['Country,Other'].replace(r'\n', '', regex=True, inplace=True)
+df = df.iloc[world_index+1:]
+df.sort_values(by='TotalCases', axis=0, ascending=False, inplace=True)
 df.head()
+
+top10_country=df.iloc[:11]['Country,Other']
+top10_country
